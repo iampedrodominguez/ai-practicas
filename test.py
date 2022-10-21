5f8981ed0c57b10dff83e099362ffe899c45a88c
Pedro Francisco Dominguez Aspilcueta <pedro.dominguez@khipu.utec.edu.pe>
2022-10-21 14:50:12 -0500

    forward (done?)

--- test.py
+++ test.py
@@ -1,17 +1,25 @@
 import numpy as np
 
 vector_weights = []
+activation_functions = []
+n_layers = 0
 
 def create_nn(layer_sizes):
     n_layers = len(layer_sizes)
     for layer in range(n_layers-1):
         vector_weights.append(np.ones((layer_sizes[layer] + 1, layer_sizes[layer + 1])))
 
-#Para una sola neurona
-def forward(X, Wt, activation_function):
-    net = np.dot(X, Wt)
-    Si = activation_function(net)
-    return Si
+#Forward para todos las capas de la red neuronal. 
+# Recibe X: Input inicial, W: Matriz con los pesos de cada capa y la lista de funciones de activación. 
+# Retorna Y: resultado de la red neuronal
+def forward_layer(X, W, activation_functions):
+    Y = X
+
+    for layer in range(n_layers - 1):
+        net = np.dot(Y, W[layer])
+        Y = activation_functions[layer](net)
+
+    return Y
 
 
 create_nn([3, 4, 3, 2])
