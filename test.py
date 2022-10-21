5840a64833c298f7e749ac2a2d013496de4d2940
Pedro Francisco Dominguez Aspilcueta <pedro.dominguez@khipu.utec.edu.pe>
2022-10-21 14:28:26 -0500

    forward draft

--- test.py
+++ test.py
@@ -5,10 +5,13 @@ vector_weights = []
 def create_nn(layer_sizes):
     n_layers = len(layer_sizes)
     for layer in range(n_layers-1):
-        vector_weights[layer] = np.ones(layer_sizes[layer] + 1, layer_sizes[layer + 1])
+        vector_weights.append(np.ones((layer_sizes[layer] + 1, layer_sizes[layer + 1])))
 
-def forward():
-    pass
+#Para una sola neurona
+def forward(X, Wt, activation_function):
+    net = np.dot(X, Wt)
+    Si = activation_function(net)
+    return Si
 
 
 create_nn([3, 4, 3, 2])
